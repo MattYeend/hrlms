c75fd3e0ea9a2a2446296156adcdd20b75400ab1
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-02-15 12:12:39 +0000

    #11 New Requests

---
+++ app/Http/Requests/UserUpdateRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'first_name' => 'required|string|max:255',
+            'middle_name' => 'nullable|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $this->route('user'),
+            'password' => 'nullable|string|min:8|confirmed', // Password is optional on update
+            'phone_number' => 'nullable|string|max:255',
+            'profile_picture' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'cv_path' => 'nullable|file|mimes:pdf,doc,docx|max:5000',
+            'cover_letter_path' => 'nullable|file|mimes:pdf,doc,docx|max:5000',
+            'salary' => 'required|integer',
+            'first_line' => 'required|string|max:255',
+            'second_line' => 'nullable|string|max:255',
+            'town' => 'nullable|string|max:255',
+            'city' => 'nullable|string|max:255',
+            'county' => 'nullable|string|max:255',
+            'country' => 'required|string|max:255',
+            'post_code' => 'required|string|max:255',
+            'full_or_part' => 'required|string|max:255',
+            'region' => 'required|string|max:255',
+            'timezone' => 'required|string|max:255',
+            'start_date' => 'required|date',
+            'end_date' => 'nullable|date',
+            'office_based' => 'nullable|integer',
+            'remote_based' => 'nullable|integer',
+            'hybrid_based' => 'nullable|integer',
+            'is_live' => 'required|boolean',
+            'role_id' => 'nullable|exists:roles,id',
+            'department_id' => 'nullable|exists:departments,id',
+            'job_title_id' => 'nullable|exists:job_titles,id',
+            'created_by' => 'nullable|exists:users,id',
+            'updated_by' => 'nullable|exists:users,id',
+            'deleted_by' => 'nullable|exists:users,id',
+        ];
+    }
+}
