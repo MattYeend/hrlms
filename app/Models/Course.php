4fe955ad295c2f088834a6c7fbc7f39e333a8813
Matt Yeend <mattyeend1@hootmail.co.uk>
2025-02-20 08:26:11 +0000

    #25 New course migration and update to course model

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,10 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Course extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable, SoftDeletes;
 
-    protected $fillable = ['title', 'description', 'thumbnail', 'category', 'duration', 'status'];
+    protected $fillable = [
+        'title', 
+        'description', 
+        'thumbnail', 
+        'category', 
+        'duration', 
+        'status',
+        'created_by',
+        'created_at',
+        'updated_by',
+        'updated_at',
+        'deleted_by',
+        'deleted_at'
+    ];
 }
---
+++ database/migrations/2025_02_20_082331_courses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->string('category')->nullable();
+            $table->integer('duration')->nullable(); // Duration in minutes
+            $table->enum('status', ['draft', 'published'])->default('draft');
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('deleted_by')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
