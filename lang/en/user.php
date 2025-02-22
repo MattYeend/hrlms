6973a96d9ddab77e216feeb535aea8e66f4df8ee
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-02-22 14:07:07 +0000

    #43 Users language file

---
+++ lang/en/user.php
@@ -0,0 +1,79 @@
+<?php
+
+use function PHPSTORM_META\map;
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | User Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following file is specific for users, whether it's user creation, 
+    | user edit, user view, or general user information. It'll include options 
+    | such as create, edit, delete, and view; amongst others.
+    |
+    */
+
+    'user_management' => 'User Management',
+    'users' => 'Users',
+    'create' => 'Create User',
+    'edit' => 'Update',
+    'view' => 'View',
+    'role' => 'Role',
+    'department' => 'Department',
+    'name' => 'Name',
+    'first_name' => 'First Name',
+    'middle_name' => 'Middle Name',
+    'last_name' => 'Last Name',
+    'email' => 'Email',
+    'first_line' => 'First Line',
+    'second_line' => 'Second Line',
+    'town' => 'Town',
+    'city' => 'City',
+    'county' => 'County',
+    'country' => 'Country',
+    'post_code' => 'Post Code',
+    'full_or_part' => 'Full Or Part Time',
+    'phone' => 'Phone',
+    'salary' => 'Salary',
+    'job' => 'Job',
+    'holiday_entitlement' => 'Holiday Entitlement',
+    'contact' => 'Contact',
+    'actions' => 'Actions',
+    'delete' => 'Delete',
+    'password' => 'Password',
+    'confirm_password' => 'Confirm Password',
+    'region' => 'Region',
+    'timezone' => 'Timezone',
+    'start_date' => 'Start Date',
+    'end_date' => 'End Date', 
+    'office' => 'Office Based',
+    'remote' => 'Remote Based',
+    'hybrid' => 'Hybrid Based',
+    'profile_picture' => 'Profile Picture',
+    'cv' => 'CV',
+    'download_cv' => 'Download CV',
+    'cover_letter' => 'Cover Letter',
+    'download_cover_letter' => 'Download Cover Letter',
+    'sensitive_documents' => 'Sensitive Documents',
+    'contains_sensitive_docs' => 'This section contains sensitive documents related to the user',
+    'select_department' => 'Select a Department',
+    'select_role' => 'Select a Role',
+    'select_job' => 'Select a Job',
+    'select_holiday_entitlement' => 'Select Holiday Entitlement',
+    'select_contact' => 'Select User Contact',
+    'full_time' => 'Full Time',
+    'part_time' => 'Part Time',
+    'select_option' => 'Select an option',
+    'not_provided' => 'Not Provided',
+    'is_live' => 'Live',
+    'is_test' => 'Test',
+    'no_users' => 'No Users',
+    'created_success' => 'User created successfully',
+    'updated_success' => 'User updated successfully',
+    'deleted_success' => 'User deleted successfully',
+    'not_found' => 'User not found',
+    'picture_upload_success' => 'Profile picture uploaded successfully',
+    'cv_upload_success' => 'CV uploaded successfully',
+    'cover_letter_upload_success' => 'Cover letter uploaded successfully',
+];
