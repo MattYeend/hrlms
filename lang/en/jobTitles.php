d51ccfe9018e6921dc318b0b4f725075a0dc990f
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-02-22 14:31:26 +0000

    #43 New job titles language file

---
+++ lang/en/jobTitles.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Job Titles Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following file is specific for job titles, whether it's job title 
+    | creation, edit, view, or general information. It'll include options such 
+    | as create, edit, delete, and view; amongst others.
+    |
+    */
+
+    'role' => 'Role',
+    'create' => 'Create a Role',
+    'edit' => 'Update',
+    'view' => 'View Role',
+    'delete' => 'Delete',
+    'name' => 'Name',
+    'not_provided' => 'Not Provided',
+    'yes' => 'Yes',
+    'no' => 'No',
+    'select_option' => 'Select an option',
+    'action' => 'Actions',
+    'no_roles' => 'No Role',
+    'unauthorized_action' => 'Unauthorized Action',
+    'created_success' => 'Role created successfully',
+    'updated_success' => 'Role updated successfully',
+    'deleted_success' => 'Role deleted successfully',
+];
