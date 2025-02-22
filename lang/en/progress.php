75a074f14135b0716d11ac67872a501023e45a00
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-02-22 14:48:04 +0000

    #43 New progress file

---
+++ lang/en/progress.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Progress Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following file is specific for Progress, whether it's creation, 
+    | edit, view, or general information. It'll include options such  as 
+    | create, edit, delete, and view; amongst others.
+    |
+    */
+
+    'progress' => 'Progress',
+    'create' => 'Create a Progress',
+    'edit' => 'Update',
+    'view' => 'View Progress',
+    'delete' => 'Delete',
+    'users' => 'Users',
+    'courses' => 'Courses',
+    'not_provided' => 'Not Provided',
+    'yes' => 'Yes',
+    'no' => 'No',
+    'select_option' => 'Select an option',
+    'action' => 'Actions',
+    'no_progress' => 'No Progress',
+    'unauthorized_action' => 'Unauthorized Action',
+    'created_success' => 'Progress created successfully',
+    'updated_success' => 'Progress updated successfully',
+    'deleted_success' => 'Progress deleted successfully',
+];
