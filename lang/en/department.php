91662eea4065a70cf30046d3d399a812fc220b4d
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-02-22 14:11:07 +0000

    #43 Department and Role Language files

---
+++ lang/en/department.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Department Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following file is specific for departments, whether it's department
+    | creation, department edit, department view, or general department
+    | information. It'll include options such as create, edit, delete, and 
+    | view; amongst others.
+    |
+    */
+
+    'department' => 'Department',
+    'create' => 'Create a Department',
+    'edit' => 'Update',
+    'view' => 'View Department',
+    'delete' => 'Delete',
+    'users' => 'Users',
+    'name' => 'Name',
+    'dept_lead' => 'Department Lead',
+    'not_provided' => 'Not Provided',
+    'yes' => 'Yes',
+    'no' => 'No',
+    'select_option' => 'Select an option',
+    'action' => 'Actions',
+    'no_departments' => 'No Departments',
+    'unauthorized_action' => 'Unauthorized Action',
+    'created_success' => 'Department created successfully',
+    'updated_success' => 'Department updated successfully',
+    'deleted_success' => 'Department deleted successfully',
+];
---
+++ lang/en/roles.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Roles Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following file is specific for roles, whether it's role creation, 
+    | role edit, role view, or general role information. It'll include options 
+    | such as create, edit, delete, and view; amongst others.
+    |
+    */
+
+    'role' => 'Role',
+    'create' => 'Create a Role',
+    'edit' => 'Update',
+    'view' => 'View Role',
+    'delete' => 'Delete',
+    'name' => 'Name',
+    'not_provided' => 'Not Provided',
+    'yes' => 'Yes',
+    'no' => 'No',
+    'select_option' => 'Select an option',
+    'action' => 'Actions',
+    'no_departments' => 'No Role',
+    'unauthorized_action' => 'Unauthorized Action',
+    'created_success' => 'Role created successfully',
+    'updated_success' => 'Role updated successfully',
+    'deleted_success' => 'Role deleted successfully',
+];
