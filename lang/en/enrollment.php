7fff0600207ec2b5fc3b215edb32a9a66dd86a0f
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-02-22 14:37:14 +0000

    #43 New enrollment language file

---
+++ lang/en/enrollment.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Enrollment Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following file is specific for enrollment, whether it's creation, 
+    | edit, view, or general information. It'll include options such  as 
+    | create, edit, delete, and view; amongst others.
+    |
+    */
+
+    'enrollment' => 'Enrollment',
+    'create' => 'Create an Enrollment',
+    'edit' => 'Update',
+    'view' => 'View Enrollment',
+    'delete' => 'Delete',
+    'users' => 'Users',
+    'courses' => 'Courses',
+    'not_provided' => 'Not Provided',
+    'yes' => 'Yes',
+    'no' => 'No',
+    'select_option' => 'Select an option',
+    'action' => 'Actions',
+    'no_course' => 'No Enrollments',
+    'unauthorized_action' => 'Unauthorized Action',
+    'created_success' => 'Enrollment created successfully',
+    'updated_success' => 'Enrollment updated successfully',
+    'deleted_success' => 'Enrollment deleted successfully',
+];
