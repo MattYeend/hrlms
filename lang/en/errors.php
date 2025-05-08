d95c6c9fec29ef4abb9202aaf10d79f881279af3
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-05-08 21:08:41 +0100

    #3 Added localization

---
+++ lang/en/errors.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    '403' => 'You do not have permission to view this page.',
+    '404' => 'The page you are looking for could not be found.',
+    '419' => 'Page expired. Please refresh and try again.',
+    '429' => 'Too many requests. Please slow down.',
+    '500' => 'Server error. We\'re working to fix it.',
+    '503' => 'Service unavailable. Please check back later.',
+];
