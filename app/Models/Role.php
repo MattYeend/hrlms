c16652098efb94f980e81e764319ddec4b989efa
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-05-26 10:26:28 +0100

    #40 Added spacing in department model and added  in role model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -13,4 +13,22 @@ class Role extends Model
     public const SUPER_ADMIN = 1;
     public const ADMIN = 2;
     public const USER = 3;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'is_default',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
