ffed456e092a0a312d58da651b8a7b6382b0bde2
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-05-30 17:00:34 +0100

    #69 New migration to add columns

---
+++ database/migrations/2025_05_30_155753_add_restored_by_and_restored_at.php
@@ -0,0 +1,74 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('restored_by')->nullable()->after('deleted_at');
+            $table->timestamp('restored_at')->nullable()->after('restored_by');
+
+            $table->foreign('restored_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+        });
+        Schema::table('departments', function (Blueprint $table) {
+            $table->unsignedBigInteger('restored_by')->nullable()->after('deleted_at');
+            $table->timestamp('restored_at')->nullable()->after('restored_by');
+
+            $table->foreign('restored_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+        });
+        Schema::table('roles', function (Blueprint $table) {
+            $table->unsignedBigInteger('restored_by')->nullable()->after('deleted_at');
+            $table->timestamp('restored_at')->nullable()->after('restored_by');
+
+            $table->foreign('restored_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+        });
+        Schema::table('companies', function (Blueprint $table) {
+            $table->unsignedBigInteger('restored_by')->nullable()->after('deleted_at');
+            $table->timestamp('restored_at')->nullable()->after('restored_by');
+
+            $table->foreign('restored_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['restored_by']);
+            $table->dropColumn(['restored_by', 'restored_at']);
+        });
+        Schema::table('departments', function (Blueprint $table) {
+            $table->dropForeign(['restored_by']);
+            $table->dropColumn(['restored_by', 'restored_at']);
+        });
+        Schema::table('roles', function (Blueprint $table) {
+            $table->dropForeign(['restored_by']);
+            $table->dropColumn(['restored_by', 'restored_at']);
+        });
+        Schema::table('companies', function (Blueprint $table) {
+            $table->dropForeign(['restored_by']);
+            $table->dropColumn(['restored_by', 'restored_at']);
+        });
+    }
+};
