57098500077351049c6b19a3de3ea781493092d8
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-06-17 14:01:19 +0100

    #164 New dashboard language file

---
+++ lang/en/dashboard.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Dashboard Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file contains the default language lines related to the dashboard.
+    | These messages are used across the application wherever dashboard data is 
+    | displayed, validated, or interacted with.
+    |
+    */
+
+    'departments' => 'Departments',
+    'archived_departments' => 'Archived Departments',
+    'active_department_text' => 'All Active Departments',
+    'archived_department_text' => 'All Archived Departments',
+    'learning_providers' => 'Learning Providers',
+    'archived_learning_providers' => 'Archived Learning Providers',
+    'active_learning_provider_text' => 'All Active Learning Providers',
+    'archived_learning_provider_text' => 'All Archived Learning Providers',
+    'users' => 'Users',
+    'archived_users' => 'Archived Users',
+    'active_user_text' => 'All Active Users',
+    'archived_user_text' => 'All Archived Users',
+    'total_blogs' => 'Total Blogs',
+    'pending_blogs' => 'Pending Blogs',
+    'approved_blogs' => 'Approved Blogs',
+    'denied_blogs' => 'Denied Blogs',
+    'total_blog_text' => 'Pending and Approved Blogs',
+    'pending_blog_text' => 'All Pending Blogs',
+    'approved_blog_text' => 'All Approved Blogs',
+    'denied_blog_text' => 'All Denied Blogs',
+];
