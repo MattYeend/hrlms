5992290002031a17626c5773614595772b61d33c
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-06-17 09:14:47 +0100

    #164 New language file for blog related information

---
+++ lang/en/blogs.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Blogs Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file contains the default language lines related to blogs.
+    | These messages are used across the application wherever blog data is 
+    | displayed, validated, or interacted with.
+    |
+    */
+
+    'name' => 'Name',
+    'contect' => 'Content',
+    'status' => 'Status',
+    'reviewed_by' => 'Reviewed By',
+    'approved' => 'Approved',
+    'approved_by' => 'Approved By',
+    'denied' => 'Denied',
+    'pending' => 'Pending',
+    'like' => 'Like',
+    'comments' => 'Comments',
+    'edit_comment' => 'Edit Comment',
+    'delete_comment' => 'Delete Comment',
+    'post_comment' => 'Post Comment',
+    'archived' => 'Archived', 
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'approve' => 'Approve',
+    'deny' => 'Deny',
+    'restore' => 'Restore',
+    'archive' => 'Archive',
+    'create' => 'Create A Blog',
+    'view' => 'View',
+    'edit' => 'Edit',
+    'actions' => 'Actions',
+    'update' => 'Update Blog',
+    'back' => 'Back',
+];
