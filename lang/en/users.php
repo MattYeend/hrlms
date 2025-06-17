fb9135f47eb03fc8de856f5c92563ced2fedb5b6
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-06-17 08:47:24 +0100

    #164 New users language file

---
+++ lang/en/users.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file contains the default language lines related to user messages.
+    | These messages are typically used by the User class for validation,
+    | notifications, or other user-related responses.
+    |
+    | You can customize these messages to better fit your application's tone
+    | or localization needs. Some messages may have multiple variations,
+    | such as those for size or format rules.
+    |
+    */
+
+    'users' => 'Users',
+    'name' => 'Name',
+    'email' => 'Email',
+    'password' => 'Password',
+    'first_line' => 'First Line',
+    'second_line' => 'Second Line',
+    'town' => 'Town',
+    'city' => 'City',
+    'county' => 'County',
+    'country' => 'Country',
+    'post_code' => 'Post Code',
+    'full_time' => 'Full Time', 
+    'part_time' => 'Part Time', 
+    'role' => 'Role',
+    'department' => 'Department', 
+    'job' => 'Job',
+    'archived' => 'Archived', 
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'restore' => 'Restore',
+    'archive' => 'Archive',
+    'create' => 'Create A User',
+    'view' => 'View',
+    'edit' => 'Edit',
+
+];
