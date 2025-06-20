ae74930dc2561eebe09932bb8d94c2224c4956eb
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-06-20 12:03:17 +0100

    #171 New language file for learning material

---
+++ lang/en/learningMaterial.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Learning Mateirals Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | This file contains the default language lines related to learning
+    | materials. These messages are used across the application wherever
+    | learning material data is displayed, validated, or interacted with.
+    |
+    */
+
+    'title' => 'Title',
+    'key_objectives' => 'Key Objectives',
+    'description' => 'Description',
+    'file_path' => 'File Path',
+    'url' => 'URL',
+    'learning_provider' => 'Learning Provider',
+    'department' => 'Department',
+    'start' => 'Start',
+    'end' => 'End',
+    'archived' => 'Archived',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'restore' => 'Restore',
+    'archive' => 'Archive',
+    'create' => 'Create A Learning Material',
+    'view' => 'View',
+    'edit' => 'Edit',
+    'actions' => 'Actions',
+    'update' => 'Update Learning Material',
+    'back' => 'Back',
+];
